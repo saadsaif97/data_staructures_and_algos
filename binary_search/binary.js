3c4c06268ef7e1e96a5444f1f9bc67b6d39c4aa2
Saad Saif <saadgfx97@gmail.com>
2021-02-11 10:15:33 +0500

    implemented binary search

--- binary_search/binary.js
+++ binary_search/binary.js
@@ -2,18 +2,21 @@ function binary_search(arr, value) {
   let left = 0
   let right = arr.length - 1
   let mid = Math.floor((left + right) / 2)
-  let m = arr[mid]
-  let l = arr[left]
-  let r = arr[right]
 
-  while (m != value || right - left == 1) {
-    if (m == value) return mid
-    if (m < value) left = mid
-    if (value < m) right = mid
+  while (arr[mid] !== value && left <= mid) {
+    console.log(left, mid, right)
+    if (arr[mid] == value) return mid
+    if (value < arr[mid]) {
+      right = mid - 1
+    } else {
+      left = mid + 1
+    }
     mid = Math.floor((left + right) / 2)
-    m = arr[mid]
   }
-  return -1
+
+  return arr[mid] == value ? mid : -1
 }
 
-console.log(binary_search([1, 2, 3, 4, 5, 6], 4))
+console.log(binary_search([1, 2, 3, 4, 5, 6], 0))
+// [1,2,3,4,5,6]
+//  l   m     r
