b0c05457bcf1a2847926f2d35fb8667bba9a5809
Saad Saif <saadgfx97@gmail.com>
2021-02-11 11:11:36 +0500

    naive string search

--- naive_string_search/string_search.js
+++ naive_string_search/string_search.js
@@ -1,13 +1,20 @@
 function string_search(long, short) {
   let counter = 0
-  for (const char of long) {
-    for (const c of short) {
-      if (c == char && c == short.slice(-1)) {
-        counter++
-      }
+
+  for (let i in long) {
+    i = parseInt(i)
+    for (let j in short) {
+      j = parseInt(j)
+      // console.log(short[j], long[i + j])
+      if (short[j] !== long[i + j]) break
+      if (short[j] === long[i + j] && j === short.length - 1) counter++
     }
   }
   return counter
 }
 
-console.log(string_search('abcdef', 'def'))
+console.log(string_search('abc f', ' f'))
+
+// abcdef
+// i + j is like sliding window
+// i + j means check from short start and long in que
